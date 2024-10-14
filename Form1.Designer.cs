8ac0f71121e30cd594f1ff767befaa4662286e85
Anaaayyy <anna.bukshtynova251106@gmail.com>
2024-10-14 11:50:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace m5_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.trackBarZoom = new System.Windows.Forms.TrackBar();
+            this.lblZoom = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox.Location = new System.Drawing.Point(229, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(538, 335);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(26, 220);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(165, 74);
+            this.btnSelectImage.TabIndex = 1;
+            this.btnSelectImage.Text = "Загрузка изображения";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click_1);
+            // 
+            // trackBarZoom
+            // 
+            this.trackBarZoom.Location = new System.Drawing.Point(37, 382);
+            this.trackBarZoom.Maximum = 100;
+            this.trackBarZoom.Minimum = 10;
+            this.trackBarZoom.Name = "trackBarZoom";
+            this.trackBarZoom.Size = new System.Drawing.Size(730, 56);
+            this.trackBarZoom.TabIndex = 2;
+            this.trackBarZoom.Value = 100;
+            this.trackBarZoom.Scroll += new System.EventHandler(this.trackBarZoom_Scroll_1);
+            // 
+            // lblZoom
+            // 
+            this.lblZoom.AutoSize = true;
+            this.lblZoom.Location = new System.Drawing.Point(34, 329);
+            this.lblZoom.Name = "lblZoom";
+            this.lblZoom.Size = new System.Drawing.Size(65, 16);
+            this.lblZoom.TabIndex = 3;
+            this.lblZoom.Text = "Масштаб";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblZoom);
+            this.Controls.Add(this.trackBarZoom);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.TrackBar trackBarZoom;
+        public System.Windows.Forms.Label lblZoom;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace m5_4
+{
+    public partial class Form1 : Form
+    {
+        private float zoomFactor = 1.0f; // Масштаб изображения
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+              
+        private void UpdateImage()
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Установка режима зума
+                pictureBox.Width = (int)(pictureBox.Image.Width * zoomFactor); // Изменение ширины
+                pictureBox.Height = (int)(pictureBox.Image.Height * zoomFactor); // Изменение высоты
+                pictureBox.Refresh(); // Обновление PictureBox
+            }
+        }
+
+        private void btnSelectImage_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Загружаем выбранное изображение
+                    pictureBox.Image = new Bitmap(openFileDialog.FileName);
+                    zoomFactor = 1.0f; // Сброс масштаба
+                    trackBarZoom.Value = 100; // Установка начального значения
+                    UpdateImage(); // Обновляем изображение
+                }
+            }
+        }
+
+        private void trackBarZoom_Scroll_1(object sender, EventArgs e)
+        {
+            zoomFactor = trackBarZoom.Value / 100.0f; // Установка нового масштаба
+            UpdateImage(); // Обновляем изображение
+            lblZoom.Text = $"Масштаб: {trackBarZoom.Value}%"; // Обновление текста метки
+        }
+    }
+}
